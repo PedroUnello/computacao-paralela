57d84f505a5565d28156bb2ded7989064c5258ac
PedroU <pedrou@pop-os.localdomain>
2021-10-14 01:14:54 -0300

    lab 6

---
+++ lab6/MultMatrizVetorPthreads.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+
+#define tam 6;
+#define qtdPthread 2;
+
+int n, m, thread_count;
+n = tam; m = tam; thread_count = (tam)/(qtdPthread);
+
+void *Pth_mat_vect( void* rank ) {
+    long my_rank = (long) rank;
+    int i, j;
+    int local_m = m/thread_count;
+    int my_first_row = my_rank*local_m;
+    int my_last_row = (my_rank+1)*local_m - 1;
+
+    for (i = my_first_row; i <= my_last_row; i++) {
+        y[i] = 0.0;
+        for (j = 0; j < n; j++)
+            y[i] += A[i][j]*x[j];
+    }
+
+    return NULL;
+}   /* Pth_mat_vect */
+
+int main(){
+    return 0;
+}
